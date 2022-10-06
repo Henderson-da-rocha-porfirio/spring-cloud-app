c1a82bd33302659d65ee907ea2abf9be42a42713
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-10-06 19:15:01 -0300

    first commit

---
+++ src/main/java/com/tuyosistema/ConfigserverBankApplication.java
@@ -0,0 +1,13 @@
+package com.tuyosistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuyosistema/ConfigserverBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyosistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
